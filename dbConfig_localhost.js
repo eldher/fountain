e5d476790dfcb50eaae600619afb9b01c80b95df
Eldher <eldher02@gmail.com>
2022-09-21 16:00:30 -0500

    PROD: Fixed Energy Transfer Upload & Dashboard

--- dbConfig_localhost.js
+++ dbConfig_localhost.js
@@ -1,41 +1,41 @@
 
-var dbConfig_localhost ={
-    user: 'admin_fountain',
-    password: 'Panama04',
-    server: 'localhost',
-    database: 'FOUNTAIN5',    
-    options: {
-        "enableArithAbort": true,
-        "encrypt":false
-    }
-
-};
-
-
-
-
-
-// var sql = require('mssql');
-
 // var dbConfig_localhost ={
 //     user: 'admin_fountain',
 //     password: 'Panama04',
-//     server: 'fountaincorp.database.windows.net',
-//     database: 'FOUNTAIN5',
-//     port : 1433,
-//     pool: {
-//         max:10,
-//         min: 0,
-//         idleTimeoutMillis: 3000
-//     },
-//     options:{
-//         encrypt: true,
-//         trustServerCertificate: true
-//         //trustedConnection: true,
-//         //enableArithPort: true,
-//         //instancename : ''
+//     server: 'localhost',
+//     database: 'FOUNTAIN5',    
+//     options: {
+//         "enableArithAbort": true,
+//         "encrypt":false
 //     }
+
 // };
+
+
+
+
+
+var sql = require('mssql');
+
+var dbConfig_localhost ={
+    user: 'admin_fountain',
+    password: 'Panama04',
+    server: 'fountaincorp.database.windows.net',
+    database: 'FOUNTAIN5',
+    port : 1433,
+    pool: {
+        max:10,
+        min: 0,
+        idleTimeoutMillis: 3000
+    },
+    options:{
+        encrypt: true,
+        trustServerCertificate: true
+        //trustedConnection: true,
+        //enableArithPort: true,
+        //instancename : ''
+    }
+};
 
 
 
