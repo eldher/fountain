6da684dc25fbc35818b428dce5004fb49de923a1
Eldher <eldher02@gmail.com>
2022-12-29 12:26:22 -0500

    PROD: Fixes and SQL for table uploads

--- sql/sp_ConsultarCargas.sql
+++ sql/sp_ConsultarCargas.sql
@@ -1,13 +1,17 @@
 -- liquidacion
 
-select fecha_mes, version, fecha_carga, count(1) as cant_registros 
+select 
+'Liquidacion' as archivo,
+fecha_mes, version, fecha_carga, count(1) as cant_registros 
 from LiquidacionFountain
 group by fecha_mes, version, fecha_carga 
 order by 1
 
 
 --
-select fecha_mes, version, fecha_carga, count(1) as cant_registros 
+select 
+'SAERLP' as archivo,
+fecha_mes, version, fecha_carga, count(1) as cant_registros 
 from SAERLP
 group by fecha_mes, version, fecha_carga 
 order by 1
@@ -15,61 +19,87 @@ order by 1
 
 
 
-select fecha_mes, version, fecha_carga, count(1) as cant_registros 
+select 
+'Servicios Auxiliares' as archivo,
+fecha_mes, version, fecha_carga, count(1) as cant_registros 
  from ServiciosAuxiliares
  group by fecha_mes, version, fecha_carga 
 order by 1
 
 
+select 
+'SASD' as archivo,
+fecha_mes, version, fecha_carga, count(1) as cant_registros 
+from SASD
+group by fecha_mes, version, fecha_carga 
+order by 1
 
-select fecha, count(1) as cant_registros
-from TotalEnergia
-group by fecha
 
 
-select fecha_mes, version, fecha_carga, count(1) as cant_registros 
+
+select 
+'Generación Obligada' as archivo,
+fecha_mes, version, fecha_carga, count(1) as cant_registros 
 from GeneracionObligada
 group by fecha_mes, version, fecha_carga 
 order by 1
 
 
 --- pendiente
-select fecha_mes, version, fecha_carga, count(1) as cant_registros 
+select 
+'Balances Potencia' as archivo,
+fecha_mes, version, fecha_carga, count(1) as cant_registros 
 from BalancesPotencia
 group by fecha_mes, version, fecha_carga 
 order by 1
 
 
 
-select fecha_fin, count(1) as cant_registros 
-from DetallePerdidas
-group by fecha_fin
-order by fecha_fin
 
 
 
-select fecha_cierre, fecha_carga, count(1) as cant_registros 
-from resumenes_generacion
-group by fecha_cierre, fecha_carga
-order by 1
-
-
-
-select fecha_mes, version, fecha_carga, count(1) as cant_registros 
-from SASD
-group by fecha_mes, version, fecha_carga 
-order by 1
-
-
-select fecha, fecha_carga,  count(1) as cant_registros 
+select 
+'Totales Contratos Energia' as archivo,
+fecha, fecha_carga,  count(1) as cant_registros 
 from TotalesContratos
 group by fecha, fecha_carga
 order by 1
 
 
 
-select fecha, fecha_carga,  count(1) as cant_registros 
+select 
+'Totales Contratos Individual' as archivo,
+fecha, fecha_carga,  count(1) as cant_registros 
 from TotalesContratos2
 group by fecha, fecha_carga
 order by 1
+
+
+
+select 
+'Resumenes Generacion' as archivo,
+fecha_cierre, fecha_carga, count(1) as cant_registros 
+from resumenes_generacion
+group by fecha_cierre, fecha_carga
+order by 1
+
+
+
+
+
+
+
+select 
+'Detalle Perdidas' as archivo,
+fecha_fin, count(1) as cant_registros 
+from DetallePerdidas
+group by fecha_fin
+order by fecha_fin
+
+
+select 
+'Total Energia' as archivo,
+fecha, count(1) as cant_registros
+from TotalEnergia
+group by fecha
 
