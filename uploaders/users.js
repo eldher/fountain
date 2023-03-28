fb258ba7488f56f830473f47055ad95cf5bb6316
Eldher <eldher02@gmail.com>
2023-03-28 17:13:17 -0500

    PROD with auth

---
+++ uploaders/users.js
@@ -0,0 +1,11 @@
+// Example users
+const users = [
+    { email: 'eldher02@gmail.com', password: '3ldh3r$fount@in' },
+    { email: 'cibar.vasquez@fountain.com.pa', password: 'cib@r$fount@in' },
+    { email: 'eyleen.espinales@fountain.com.pa', password: '3yl33n$fount@in' },
+    { email: 'ian.dillon@fountain.com.pa', password: 'i@n$fount@in' }
+  ];
+  
+
+
+  module.exports = users;
