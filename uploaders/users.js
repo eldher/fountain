3c01bbb4e6d0e2d1724d76048d79dabec54ec1dd
Eldher <eldher02@gmail.com>
2023-05-03 11:03:50 -0500

    PROD: added users

--- uploaders/users.js
+++ uploaders/users.js
@@ -3,7 +3,11 @@ const users = [
     { email: 'eldher02@gmail.com', password: '3ldh3r$fount@in' },
     { email: 'cibar.vasquez@fountain.com.pa', password: 'cib@r$fount@in' },
     { email: 'eyleen.espinales@fountain.com.pa', password: '3yl33n$fount@in' },
-    { email: 'ian.dillon@fountain.com.pa', password: 'i@n$fount@in' }
+    { email: 'ian.dillon@fountain.com.pa', password: 'i@n$fount@in' },
+    { email: 'ian.dillon@fountain.com.pa', password: 'i@n$fount@in' },
+    { email: 'ted@plazareg.com', password: 't3d$fount@in' },
+    { email: 'kevin.taylor@aguaimara.com', password: 'k3v1n$fount@in' }
+
   ];
   
 
